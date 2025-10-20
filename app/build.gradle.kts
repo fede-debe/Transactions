2666650bca2936f68a9dfe890027ac22b06d71ce
“fede-debe” <federico.debenedictis33@gmail.com>
2025-10-20 20:06:33 +0200

    feat: Initial project setup
    - Add `streamfetch` and `datasyncflow` modules.
    - Update dependencies for Compose, Hilt, Room, and Coroutines.
    - Configure plugins for KSP, serialization, and Hilt.
    - Set Java and Kotlin versions to 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -30,31 +33,72 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
 }
 
+hilt {
+    enableAggregatingTask = false
+}
+
 dependencies {
+    //local
+//    implementation(project(":dataloader"))
+
+    // Core AndroidX + Kotlinx Serialization
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    // --- Minimal Compose stack ---
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.compose.placeholder.material3)
+
+
+    // ViewModel + lifecycle for Compose
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    // Navigation Compose
+    implementation(libs.androidx.navigation.compose)
+
+    // Coroutines runtime (for ViewModel/repo)
+    implementation(libs.kotlinx.coroutines.android)
+
+    //room
+    implementation(libs.bundles.androidx.room)
+    ksp(libs.androidx.room.compiler)
+
+    // hilt
+    implementation(libs.hilt.android.core)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Debug-only compose tools
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.kotlinx.serialization.json)
+    testImplementation(libs.androidx.test.ext.junit)
+    testImplementation(libs.androidx.espresso.core)
+
+    coreLibraryDesugaring(libs.android.desugarJdkLibs)
 }
